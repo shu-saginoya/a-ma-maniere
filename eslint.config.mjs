4b47b02c0f63bb9fa7a017e373d0e5027394763e
shu.saginoya <shu.saginoya@gmail.com>
2026-09-17 20:30:00 +0900

    ESLint(Astro/TypeScript)を追加

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist/', '.astro/'] },
+  tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+);
